a6f9b9a935254c027f3977e1d04301d108e55bd9
Dusan Trtica <dusan.trtica@hotmail.com>
2026-01-22 10:17:29 +0100

    Tseitin transformation, not finished

--- src/model/clause.py
+++ src/model/clause.py
@@ -39,3 +39,54 @@ def cnf (formula: Formula) -> NormalForm:
                 case BinaryData.OR:
                     return cross(cnf(formula.data.left), cnf(formula.data.right))
                 
+
+class AtomWrapper:
+    def __init__(self, value):
+        self.n = value
+
+    def return_and_increment(self):
+        self.n += 1
+        return self.n-1
+
+
+
+def tseitin_substitution(formula: Formula, normal_form: NormalForm, atom_wrapper: AtomWrapper) -> int:
+    match formula.formula_type:
+        case Type.ATOM:
+            return formula.data
+        case Type.TRUE:
+            normal_form.append([Literal(True, atom_wrapper.n)])
+            return atom_wrapper.return_and_increment()
+        case Type.FALSE:
+            normal_form.append([Literal(False, atom_wrapper.n)])
+            return atom_wrapper.return_and_increment()
+        case Type.NOT:
+            sub_formula = tseitin_substitution(formula.data, normal_form, atom_wrapper)
+            tseitin_not = [
+                [Literal(False, sub_formula), Literal(False, atom_wrapper.n)],
+                [Literal(True, sub_formula), Literal(True, atom_wrapper.n)]]
+            normal_form.append(tseitin_not)
+
+            return atom_wrapper.return_and_increment()
+
+        case Type.BINARY:                            # left and right are substitution letters, integers
+            left = tseitin_substitution(formula.data.left, normal_form, atom_wrapper)
+            right = tseitin_substitution(formula.data.right, normal_form, atom_wrapper)
+            match formula.data.binary_type:
+                case BinaryData.Type.AND:
+                    tseitin_binary = [
+                        [Literal(False, atom_wrapper.n), Literal(True, left)],
+                        [Literal(False, atom_wrapper.n), Literal(True, right)],
+                        [Literal(True, atom_wrapper.n), Literal(False, left), Literal(False, right)]
+                    ]
+                    normal_form.append(*tseitin_binary)
+                    return atom_wrapper.return_and_increment()
+
+                    
+def tseitin(formula: Formula) -> NormalForm:
+    normal_form = []
+    atom_count = atom_count(formula)
+    atom_wrapper = AtomWrapper(atom_count+1)
+    s = tseitin_substitution(formula, normal_form, atom_wrapper)
+    normal_form.append([Literal(True, s)])
+    return normal_form
